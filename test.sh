700e3df8d5f09096323a93f0a07c2df86d1bb14e
OHASHI Hideya <ohachige@gmail.com>
2011-01-19 12:49:41 +0900

    add simple test

---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+rm -fr sandbox
+mkdir sandbox
+cd sandbox
+
+../bin/ib init
+echo 1 >1
+echo 2 >2
+echo 3 >3
+
+../bin/ib st >actual
+cat <<EOF >.ib/expected
+? 1
+? 2
+? 3
+EOF
+diff -u .ib/expected actual
+
+ACTUAL=`../bin/ib log | wc -l`
+if [ $ACTUAL -ne 0 ]; then
+  echo $ACTUAL
+fi
+
+../bin/ib ci
+
+ACTUAL=`../bin/ib log | wc -l`
+if [ $ACTUAL -ne 1 ]; then
+  echo $ACTUAL
+fi
+
+../bin/ib ci
+
+ACTUAL=`../bin/ib log | wc -l`
+if [ $ACTUAL -ne 1 ]; then
+  echo $ACTUAL
+fi
+
+ACTUAL=`../bin/ib st | wc -l`
+if [ $ACTUAL -ne 0 ]; then
+  echo $ACTUAL
+fi
+
+rm 2
+../bin/ib st >actual
+cat <<EOF >.ib/expected
+M 2
+EOF
+diff -u .ib/expected actual
+
+../bin/ib up
+ACTUAL=`../bin/ib st | wc -l`
+if [ $ACTUAL -ne 0 ]; then
+  echo $ACTUAL
+fi
